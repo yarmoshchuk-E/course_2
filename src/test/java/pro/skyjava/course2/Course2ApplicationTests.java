d70a4965d8684f6e1c8ec3dcc03a5ec037ebd080
yarmoshchuk-E <yarmoshchuk_80@mail.ru>
2026-06-17 22:45:19 +0400

    initial commit

---
+++ src/test/java/pro/skyjava/course2/Course2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.skyjava.course2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
